b89c6ee672d6e6934837a824326c0de2535634f5
root <root@localhost.localdomain>
2017-12-29 16:41:26 -0300

    Script Creado

---
+++ ProxyListFormat.py
@@ -0,0 +1,28 @@
+import sys
+
+# Leer una lista de proxys con el formato ip:puerto y darle el formato
+# <tipo> <ip> <puerto>
+# para añadir a proxichains config.
+
+ruta = sys.argv[1]
+tipo_prox = sys.argv[2]
+
+def ParcearArchivo(ruta_ar,tipo_p):
+    # Abrir archivo en modo lectura
+    print(ruta_ar)
+    print(tipo_p)
+    archivo_poxys = open(ruta_ar,'r')
+    lineas_de_archivo = archivo_poxys.readlines()
+    lista_proxys = []
+    for x in lineas_de_archivo:
+        cad = "{tipo} {ip} {puerto}"
+        ip_prox = x.split(":")[0]
+        puerto_prox = x.split(":")[1].strip("\n")
+        proxy = cad.format(tipo=tipo_p, ip=ip_prox, puerto=puerto_prox)
+        lista_proxys.append(proxy)
+    return lista_proxys
+
+lista_prox = ParcearArchivo(ruta,tipo_prox)
+
+for proxy in lista_prox:
+    print(proxy)
